64d759e7459c1e69cdbae5871b46b33bf07156f8
pankajkrravi <pankajravi25@gmail.com>
2021-10-16 19:41:52 +0530

    sum_of_square_evenNumbers

--- src/com/java/hackerrank/GFG/mathematical/PowerOfPowEvenNumber.java
+++ src/com/java/hackerrank/GFG/mathematical/PowerOfPowEvenNumber.java
@@ -5,5 +5,19 @@ package com.java.hackerrank.GFG.mathematical;
  * @create 10/16/21 5:44 PM
  */
 public class PowerOfPowEvenNumber {
-    
+    public long sum_of_square_evenNumbers(long n)
+    {
+        // Code here
+        long i=1,sum=0,square=0;
+        while (i <=n){
+            square=square+2;
+            sum =sum+(square*square);
+            i++;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new PowerOfPowEvenNumber().sum_of_square_evenNumbers(2));
+    }
 }
